97ccce71e8efd040a1567a8c81fa94d5e7f8a1a3
Sean L <71533878+Sean02@users.noreply.github.com>
2022-12-14 14:15:11 -0800

    Added some basic login functions
    Having errors with the imports on my side

--- public/app.js
+++ public/app.js
@@ -1,5 +1,54 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import "firebase/auth";
+// import { getAuth } from "firebase/auth";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 document.addEventListener("DOMContentLoaded", event => {
 
-    const app = firebase.app()
+    const app = firebase.app();
+    console.log(app)
 
 });
+
+
+const auth = firebase.auth();
+// firebase.auth().createUserWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     var user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ..
+//   });
+
+
+
+function createEmailLogin(){
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+}
+
+function emailLogin(){
+    // const provider = new Firebase.auth.emailAu
+
+
+
+}
+
+
+function anonLogin(){
+
+}
